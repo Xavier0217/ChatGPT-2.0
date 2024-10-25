2d21eebfce5aa05178932d9bab2510bec6c1592c
Xavier017 <arthur.xavier016002@gmail.com>
2024-10-25 08:43:53 -0300

    1º envio chatGames

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ChatGPT-2.0
 clone do chat gpt
+
+## Bibliotecas utilizadas
+```pip install Flask```
+```pip install python-dotenv```
+```pip install google_generativeai```
---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask,render_template, request
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+
+load_dotenv()
+
+app = Flask(__name__)
+CONTEXT = 'Responda como um Jogador de Games'
+error_message =''
+
+try:
+    model = genai.GenerativeModel('gemini-1.0-pro-latest')
+    genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
+except Exception as error:
+    error_message = f'Erro ao carregar o modelo: {error}'
+@app.route('/')
+def index():
+    if len(error_message) == 0:
+        return render_template("index.html")
+    else:
+        return{'messsage': error_message}
+
+@app.route('/search')
+def search():
+    prompt = request.args.get('prompt')
+    try:
+        imput_ia = f'{CONTEXT}: {prompt}'
+        output = model.generate_content(imput_ia)
+        return {'message': output.text}
+    except Exception as ex:
+        print(f'Erro ao gerar resposta: {ex}')
+        return 'Ocorreu um erro ao processar sua solicitação :(z'
+
+if __name__ == '__main__':
+    app.run(debug=True)
